1bc31d0ad90a36cb40c8c3379b823bbab23bfe75
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2023-05-21 17:34:44 +0000

    Use helper functions

--- language-detection.go
+++ language-detection.go
@@ -6,12 +6,41 @@ package main
 // #include <stdlib.h>
 import "C"
 import (
+	"fmt"
 	"unsafe"
 
 	"github.com/abadojack/whatlanggo"
 )
 
 func main() {}
+
+func GetArgumentAsString(args *C.HalonHSLArguments, pos uint64) (string, error) {
+	var x = C.HalonMTA_hsl_argument_get(args, C.ulong(pos))
+	if x == nil {
+		return "", fmt.Errorf("missing argument at position %d", pos)
+	}
+	var y *C.char
+	if C.HalonMTA_hsl_value_get(x, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&y), nil) {
+		return C.GoString(y), nil
+	} else {
+		return "", fmt.Errorf("invalid argument at position %d", pos)
+	}
+}
+
+func SetException(hhc *C.HalonHSLContext, msg string) {
+	x := C.CString(msg)
+	y := unsafe.Pointer(x)
+	defer C.free(y)
+	exception := C.HalonMTA_hsl_throw(hhc)
+	C.HalonMTA_hsl_value_set(exception, C.HALONMTA_HSL_TYPE_EXCEPTION, y, 0)
+}
+
+func SetReturnValueToString(ret *C.HalonHSLValue, val string) {
+	x := C.CString(val)
+	y := unsafe.Pointer(x)
+	defer C.free(y)
+	C.HalonMTA_hsl_value_set(ret, C.HALONMTA_HSL_TYPE_STRING, y, 0)
+}
 
 //export Halon_version
 func Halon_version() C.int {
@@ -25,36 +54,14 @@ func Halon_init(hic *C.HalonInitContext) C.bool {
 
 //export detect_language
 func detect_language(hhc *C.HalonHSLContext, args *C.HalonHSLArguments, ret *C.HalonHSLValue) {
-	var text string
-	var text_cs *C.char
-
-	var args_0 = C.HalonMTA_hsl_argument_get(args, 0)
-	if args_0 != nil {
-		if !C.HalonMTA_hsl_value_get(args_0, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&text_cs), nil) {
-			exception := C.HalonMTA_hsl_throw(hhc)
-			value_cs := C.CString("Invalid type of \"text\" argument")
-			value_cs_up := unsafe.Pointer(value_cs)
-			defer C.free(value_cs_up)
-			C.HalonMTA_hsl_value_set(exception, C.HALONMTA_HSL_TYPE_EXCEPTION, value_cs_up, 0)
-			return
-		}
-		text = C.GoString(text_cs)
-	} else {
-		exception := C.HalonMTA_hsl_throw(hhc)
-		value_cs := C.CString("Missing required \"text\" argument")
-		value_cs_up := unsafe.Pointer(value_cs)
-		defer C.free(value_cs_up)
-		C.HalonMTA_hsl_value_set(exception, C.HALONMTA_HSL_TYPE_EXCEPTION, value_cs_up, 0)
+	text, err := GetArgumentAsString(args, 0)
+	if err != nil {
+		SetException(hhc, err.Error())
 		return
 	}
-
 	info := whatlanggo.Detect(text)
 	language := info.Lang.String()
-	language_cs := C.CString(language)
-	language_cs_up := unsafe.Pointer(language_cs)
-	defer C.free(language_cs_up)
-
-	C.HalonMTA_hsl_value_set(ret, C.HALONMTA_HSL_TYPE_STRING, language_cs_up, 0)
+	SetReturnValueToString(ret, language)
 }
 
 //export Halon_hsl_register
